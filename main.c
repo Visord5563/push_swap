665cd378a9217aa415adc855b7193b67c033e959
Salah-Eddine Harchiche <saharchi@e3r11p8.1337.ma>
2024-02-14 11:28:57 +0100

    edit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: saharchi <saharchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 19:51:25 by saharchi          #+#    #+#             */
-/*   Updated: 2024/02/14 06:55:55 by saharchi         ###   ########.fr       */
+/*   Updated: 2024/02/14 11:27:58 by saharchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,50 +101,128 @@ void	stack_clear(t_stack **lst)
 	}
 }
 
-void	sa(t_stack *list)
+void	sa(t_stack *a)
 {
 	int	tmp;
 
-	tmp = list->content;
-	list->content = list->next->content;
-	list->next->content = tmp;
+	tmp = a->content;
+	a->content = a->next->content;
+	a->next->content = tmp;
 	write(1, "sa\n", 3);
 }
 
-void	sb(t_stack *list)
+void	sb(t_stack *b)
 {
 	int	tmp;
 
-	tmp = list->content;
-	list->content = list->next->content;
-	list->next->content = tmp;
+	tmp = b->content;
+	b->content = b->next->content;
+	b->next->content = tmp;
 	write(1, "sb\n", 3);
 }
 
-void ra(t_stack **list)
+void	ss(t_stack *a, t_stack *b)
+{
+	sa(a);
+	sb(b);
+	write(1, "ss\n", 3);
+}
+
+void ra(t_stack **a)
 {
 	t_stack	*tmp;
 	t_stack	*curnt;
 
-	tmp = *list;
-	curnt = ft_lstlast(*list);
+	tmp = *a;
+	curnt = ft_lstlast(*a);
 	curnt->next = tmp;
-	(*list) = tmp->next;
+	(*a) = tmp->next;
 	tmp->next = NULL;
 	write(1, "ra\n", 3);
 }
 
-void rb(t_stack **list)
+void rb(t_stack **b)
 {
 	t_stack	*tmp;
 	t_stack	*curnt;
 
-	tmp = *list;
-	curnt = ft_lstlast(*list);
+	tmp = *b;
+	curnt = ft_lstlast(*b);
 	curnt->next = tmp;
-	(*list) = tmp->next;
+	(*b) = tmp->next;
 	tmp->next = NULL;
 	write(1, "rb\n", 3);
+}
+
+void	ft_lstadd_front(t_stack **lst, t_stack *new)
+{
+	if (!new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+void	rra(t_stack **a)
+{
+	t_stack *tmp;
+	t_stack *curnt;
+	
+	curnt = *a;
+	tmp = ft_lstlast(*a);
+	while (curnt->next->next)
+		curnt = curnt->next;
+	curnt->next = NULL;
+	ft_lstadd_front(a, tmp);
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_stack **b)
+{
+	t_stack *tmp;
+	t_stack *curnt;
+	
+	curnt = *b;
+	tmp = ft_lstlast(*b);
+	while (curnt->next->next)
+		curnt = curnt->next;
+	curnt->next = NULL;
+	ft_lstadd_front(b, tmp);
+	write(1, "rrb\n", 4);
+}
+
+void	pb(t_stack **a, t_stack **b)
+{
+	t_stack *tmp;
+
+	tmp = *a;
+	*a = (*a)->next;
+	ft_lstadd_front(b, tmp);
+	write(1, "pb\n", 3);
+}
+
+void	pa(t_stack **a, t_stack **b)
+{
+	t_stack *tmp;
+
+	tmp = *b;
+	*b = (*b)->next;
+	ft_lstadd_front(a, tmp);
+	write(1, "pa\n", 3);
+}
+
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	rra(a);
+	rrb(b);
+	write(1, "rrr\n", 4);
+}
+
+void	rr(t_stack **a, t_stack **b)
+{
+	ra(a);
+	rb(b);
+	write(1, "rr\n", 3);
 }
 
 int	main(int ac, char **av)
@@ -152,6 +230,7 @@ int	main(int ac, char **av)
 	char		*agv;
 	int			i;
 	t_stack		*a;
+	t_stack		*b;
 
 	a = NULL;
 	i = 1;
@@ -177,11 +256,22 @@ int	main(int ac, char **av)
 		ft_lstadd_back(&a, ft_lstnew(ft_atoi(av[i])));
 		i++;
 	}
-	ra(&a);
+	pb(&a, &b);
+	pb(&a, &b);
+	pb(&a, &b);
+	pb(&a, &b);
+	pa(&a, &b);
 	while (a)
 	{
 		printf("%d\n", a->content);
+		// while(1);
 		a = a->next;
+	}
+	while(b)
+	{
+		printf("b=%d\n", b->content);
+		// while(1);
+		b = b->next;
 	}
 	return (0);
 }
